949fd06c5cf36c471bc3f50d2de5e9c3a4f76660
Travis Klotz <travis.klotz@gmail.com>
2022-11-30 22:16:51 -0600

    Init

---
+++ src/test/kotlin/Day1.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day1 {
+
+    @Test
+    fun part1() {
+
+    }
+
+    @Test
+    fun part2() {
+
+    }
+
+    fun loadData() {
+        val lines = File(ClassLoader.getSystemResource("day1.txt").file).readLines()
+    }
+}
