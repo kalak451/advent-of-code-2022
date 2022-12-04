52f1ac68a8725b956dc96aa92ea4f289d9dafc5e
Travis Klotz <travis.klotz@gmail.com>
2022-12-03 22:09:56 -0600

    Add shortest path helper (I know I'm going to need it eventually.

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun <T> Sequence<T>.delimited(predicate: (T, T) -> Boolean): Sequence<List<T>> {
     val underlying = this
     return sequence {
@@ -16,4 +18,26 @@ fun <T> Sequence<T>.delimited(predicate: (T, T) -> Boolean): Sequence<List<T>> {
             yield(buffer)
         }
     }
+}
+
+
+private fun <T> shortestPath(start: T, adj: (T) -> List<Pair<T, Long>>): Map<T, Long> {
+    val dist: MutableMap<T, Long> = mutableMapOf()
+    dist[start] = 0
+
+    val queue = PriorityQueue<Pair<T, Long>>(compareBy { it.second })
+    queue.add(Pair(start, 0))
+
+    while (queue.isNotEmpty()) {
+        val (t, _) = queue.poll()
+        adj(t).forEach { (newT, cost) ->
+            val distToNewT = dist[t]!! + cost
+            if (distToNewT < (dist[newT] ?: Long.MAX_VALUE)) {
+                dist[newT] = distToNewT
+                queue.add(Pair(newT, distToNewT))
+            }
+        }
+    }
+
+    return dist.toMap()
 }
