845ed533a86ba59d5a80a1f06a02a3a8d05e6eb0
Travis Klotz <travis.klotz@gmail.com>
2022-12-07 12:01:52 -0600

    Day 7 cleanup

--- src/test/kotlin/Day7.kt
+++ src/test/kotlin/Day7.kt
@@ -11,8 +11,7 @@ class Day7 {
 
         val answer = allDirectories
             .filter { x -> x.size() <= 100000 }
-            .map { x -> x.size() }
-            .sum()
+            .sumOf { x -> x.size() }
 
         assertEquals(1783610, answer)
     }
@@ -31,8 +30,8 @@ class Day7 {
 
         val allDirectories = root.allChildren()
         val dirToDelete = allDirectories
-            .sortedBy { it.size() }
-            .first { it.size() >= needToDelete }
+            .filter { it.size() >= needToDelete }
+            .minBy { it.size() }
 
 
         val answer = dirToDelete.size()
@@ -53,16 +52,16 @@ class Day7 {
                 val results = lines.drop(1)
 
                 if (cmd.startsWith("$ ls")) {
-                    current = processLsCommand(cmd, results, current)
+                    current = processLsCommand(results, current)
                 } else if (cmd.startsWith("$ cd")) {
-                    current = processCdCommand(cmd, results, current)
+                    current = processCdCommand(cmd, current)
                 }
             }
 
         return root
     }
 
-    private fun processCdCommand(cmd: String, results:List<String>, current: DDirectory): DDirectory {
+    private fun processCdCommand(cmd: String, current: DDirectory): DDirectory {
         val nextDir = cmd.substring(5)
         return if (nextDir == "..") {
             current.parent!!
@@ -71,7 +70,7 @@ class Day7 {
         }
     }
 
-    private fun processLsCommand(cmd: String, results: List<String>, current: DDirectory): DDirectory {
+    private fun processLsCommand(results: List<String>, current: DDirectory): DDirectory {
         results.forEach { r ->
             if (r.startsWith("dir ")) {
                 val dName = r.substring(4)
@@ -95,13 +94,13 @@ class Day7 {
     ) {
 
         fun size(): Long {
-            val fileSize = files.values.map { it.size }.sum()
-            val childSize = children.values.map { it.size() }.sum()
+            val fileSize = files.values.sumOf { it.size }
+            val childSize = children.values.sumOf { it.size() }
 
             return fileSize + childSize
         }
-        fun allChildren(): List<DDirectory> {
-            return children.values + children.values.flatMap { c -> c.allChildren() }
+        fun allChildren(): Sequence<DDirectory> {
+            return children.values.asSequence() + children.values.asSequence().flatMap { c -> c.allChildren() }
         }
     }
 
